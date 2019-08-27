a8fcc33e5af4cfc6a85c738716a03b1ac0354c28
Sarah Angelici <sangelici94@gmail.com>
2019-08-26 21:39:18 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,9 +1,26 @@
 console.log("Up and running!");
 
-var cardOne = "Queen";
-var cardTwo = "Queen";
-var cardThree = "King";
-var cardFour = "King";
+// var cardOne = "Queen";
+// var cardTwo = "Queen";
+// var cardThree = "King";
+// var cardFour = "King";
+// console.log("User flipped " + cardOne);
+// console.log("User flipped " + cardThree);
 
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardThree);
+var cards = ["Queen", "Queen", "King", "King"];
+var cardsInPlay = [];
+var cardOne = cards[0];
+var cardTwo = cards[2];
+
+cardsInPlay.push(cardOne);
+cardsInPlay.push(cardTwo);
+
+console.log("User flipped " + cardsInPlay)
+
+if (cardsInPlay.length === 2){
+  if (cardsInPlay[0] === cardsInPlay[1]) {
+    alert("You found a match!");
+  } else {
+    alert("Sorry, try again.");
+  }
+}
